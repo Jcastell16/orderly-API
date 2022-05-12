d9296eea1c19b4e3bd88e282f8887506829e4cc0
dayanna <dayanna_garcia@hotmail.com>
2022-05-12 19:08:08 -0400

    [CREATE], created register endpoint

--- src/main.py
+++ src/main.py
@@ -8,7 +8,7 @@ from flask_swagger import swagger
 from flask_cors import CORS
 from utils import APIException, generate_sitemap
 from admin import setup_admin
-from models import db, User
+from models import db, User, Profile
 #from models import Person
 
 app = Flask(__name__)
@@ -36,8 +36,43 @@ def handle_hello():
     response_body = {
         "msg": "Hello, this is your GET /user response "
     }
-
     return jsonify(response_body), 200
+
+@app.route("/register", methods=["POST"])
+def register_user():
+    email = request.json.get("email", None)
+    password = request.json.get("password", None)
+    name = request.json.get("name", None)
+    lastname = request.json.get("lastname", None)
+    if email is None:
+        return jsonify("Please provide a valid email."), 400
+    if password is None:
+        return jsonify("Please provide a valid password."), 400
+    if name is None:
+        return jsonify("Please provide a valid name."), 400  
+    if lastname is None:
+        return jsonify("Please provide a valid lastname."), 400    
+    user = User.query.filter_by(email=email).first()
+    if user:
+        return jsonify("User already exists."), 401
+    else:
+        newuser = User()
+        newuser.email = email
+        newuser.password = password
+        db.session.add(newuser)
+        db.session.commit()
+
+        userid = User.query.filter_by(email=email).first()
+        if userid:
+            newprofile = Profile()
+            newprofile.user_id = userid.id
+            newprofile.name = name
+            newprofile.lastname = lastname
+            db.session.add(newprofile)
+            db.session.commit()
+            return jsonify({"msg": "User account was successfully created."}), 200
+        else:
+            return jsonify("User wasn't successfully create"), 401
 
 # this only runs if `$ python src/main.py` is executed
 if __name__ == '__main__':
