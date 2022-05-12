4ed4c71a8dfda585bb5674aa7f341f4708a57d9d
Gabriel Perez <gabrielperezpernia@gmail.com>
2022-05-12 18:52:47 -0400

    [CREATE] endpoint login

--- src/main.py
+++ src/main.py
@@ -5,6 +5,7 @@ import os
 from flask import Flask, request, jsonify, url_for
 from flask_migrate import Migrate
 from flask_swagger import swagger
+from flask_jwt_extended import JWTManager, create_access_token, get_jwt_identity, jwt_required
 from flask_cors import CORS
 from utils import APIException, generate_sitemap
 from admin import setup_admin
@@ -15,6 +16,8 @@ app = Flask(__name__)
 app.url_map.strict_slashes = False
 app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DB_CONNECTION_STRING')
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+app.config['JWT_SECRET_KEY']=os.environ.get('FLASK_APP_KEY')
+jwt = JWTManager(app)
 MIGRATE = Migrate(app, db)
 db.init_app(app)
 CORS(app)
@@ -38,7 +41,27 @@ def handle_hello():
     }
 
     return jsonify(response_body), 200
-
+@app.route('/login', methods=['GET'])
+def handle_login():
+    email = request.json.get("email", None)
+    password = request.json.get("password", None)
+    if email is not None and password is not None:
+        user = User.query.filter_by(email=email, password=password).one_or_none()
+        if user is not None:
+            create_token = create_access_token(identity=user.id)
+            return jsonify({
+                "token": create_token,
+                "user.id": user.id,
+                "email": user.email
+            }), 200
+        else:
+           return jsonify({
+            "msg": "not found"
+        }),404  
+    else:
+        return jsonify({
+            "msg": "error"
+        }),400
 # this only runs if `$ python src/main.py` is executed
 if __name__ == '__main__':
     PORT = int(os.environ.get('PORT', 3000))
